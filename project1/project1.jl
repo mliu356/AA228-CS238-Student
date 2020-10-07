5eb670a2f03cc54d2a5ca8c7c9a64202577dadd6
Michelle <michelleliu343@gmail.com>
2020-10-07 15:15:43 -0700

    write bayesian score
    i hope i'm not writing to the public code

--- project1/project1.jl
+++ project1/project1.jl
@@ -14,13 +14,119 @@ function write_gph(dag::DiGraph, idx2names, filename)
     end
 end
 
+# struct K2Search
+#     ordering::Vector{Int} # variable ordering
+# end
+
+# function fit(method::K2Search, vars, D)
+#     G = SimpleDiGraph(length(vars))
+#     for (k,i) in enumerate(method.ordering[2:end])
+#         y = bayesian_score(vars, G, D)
+#         while true
+#             y_best, j_best = -Inf, 0
+#             for j in method.ordering[1:k]
+#                 if !has_edge(G, j, i)
+#                     add_edge!(G, j, i)
+#                     y′ = bayesian_score(vars, G, D)
+#                     if y′ > y_best
+#                         y_best, j_best = y′, j
+#                     end
+#                     rem_edge!(G, j, i)
+#                 end
+#             end
+#             if y_best > y
+#                 y = y_best
+#                 add_edge!(G, j_best, i)
+#             else
+#                 break
+#             end
+#         end
+#     end
+#     return G
+# end
+
+struct LocalDirectedGraphSearch
+    G # initial graph
+    k_max # number of iterations
+end
+
+function rand_graph_neighbor(G)
+    n = nv(G)
+    i = rand(1:n)
+    j = mod1(i + rand(2:n)-1, n)
+    G′ = copy(G)
+    has_edge(G, i, j) ? rem_edge!(G′, i, j) : add_edge!(G′, i, j)
+    return G′
+end
+
+function fit(method::LocalDirectedGraphSearch, vars, D)
+    G = method.G
+    y = bayesian_score(vars, G, D)
+    for k in 1:method.k_max
+        G′ = rand_graph_neighbor(G)
+        y′ = is_cyclic(G′) ? -Inf : bayesian_score(vars, G′, D)
+        if y′ > y
+            y, G = y′, G′
+        end
+    end
+    return G
+end
+
+function sub2ind(siz, x)
+    k = vcat(1, cumprod(siz[1:end-1]))
+    return dot(k, x .- 1) + 1
+end
+
+function statistics(vars, G, D::Matrix{Int})
+    n = size(D, 1)
+    r = [vars[i].m for i in 1:n]
+    q = [prod([r[j] for j in inneighbors(G,i)]) for i in 1:n]
+    M = [zeros(q[i], r[i]) for i in 1:n]
+    for o in eachcol(D)
+        for i in 1:n
+            k = o[i]
+            parents = inneighbors(G,i)
+            j = 1
+            if !isempty(parents)
+                j = sub2ind(r[parents], o[parents])
+            end
+            M[i][j,k] += 1.0
+        end
+    end
+    return M
+end
+
+function prior(vars, G)
+    n = length(vars)
+    r = [vars[i].m for i in 1:n]
+    q = [prod([r[j] for j in inneighbors(G,i)]) for i in 1:n]
+    return [ones(q[i], r[i]) for i in 1:n]
+end
+
+function bayesian_score_component(M, α)
+    p = sum(loggamma.(α + M))
+    p -= sum(loggamma.(α))
+    p += sum(loggamma.(sum(α,dims=2)))
+    p -= sum(loggamma.(sum(α,dims=2) + sum(M,dims=2)))
+    return p
+end
+
+function bayesian_score(vars, G, D)
+    n = length(vars)
+    M = statistics(vars, G, D)
+    α = prior(vars, G)
+    return sum(bayesian_score_component(M[i], α[i]) for i in 1:n)
+end
 
 function compute(infile, outfile)
+    # process infile into graph (all unconnected nodes?)
 
-    # WRITE YOUR CODE HERE
-    # FEEL FREE TO CHANGE ANYTHING ANYWHERE IN THE CODE
-    # THIS INCLUDES CHANGING THE FUNCTION NAMES, MAKING THE CODE MODULAR, BASICALLY ANYTHING
+    # loop while not converged
+        # run local search?
+        # calculate new bayesian score
+        # if different, keep going, else break
 
+    # write graph to outfile using write_gph
 end
 
 if length(ARGS) != 2
--- project1/project1.py
+++ project1/project1.py
@@ -1,28 +1,109 @@
 import sys
-
-import networkx
-
+import networkx as nx
+import pandas as pd
+import numpy as np
+from scipy.special import loggamma 
 
 def write_gph(dag, idx2names, filename):
     with open(filename, 'w') as f:
         for edge in dag.edges():
             f.write("{}, {}\n".format(idx2names[edge[0]], idx2names[edge[1]]))
 
+def process(graph: nx.Graph, data: pd.DataFrame):
+    values = {} # node -> {values}
+    parents = {} # node -> [parents]
+    for node in graph.nodes():
+        values[node] = sorted(data[node].unique().tolist())
+        parents[node] = []
+    for edge in graph.edges():
+        parent, child = edge
+        parents[child] += [parent]
+    return values, parents
+
+def get_ijk_value(index: str, row: pd.Series, values: {}, parents: {}, nodes_list: []):
+    curr_var = nodes_list[index]
+    x_parents = parents[curr_var].copy()
+    x_parents_vals = [row.iloc[nodes_list.index(parent)] for parent in x_parents]
+    x_parents_dims = [len(values[parent]) for parent in x_parents]
+    j = (np.ravel_multi_index(x_parents_vals, x_parents_dims) if len(x_parents) > 0 else 0)
+    k = values[curr_var].index(row.iloc[index])
+    return index, j, k
+
+def bayesian_score(graph: nx.Graph, data: pd.DataFrame):
+    values, parents = process(graph, data)
+    nodes = list(graph.nodes)
+
+    # * instantiate empty counts : list of 2d arrays
+    counts = [None] * len(nodes)
+    for index in range(len(counts)):
+        curr_var = nodes[index]
+        q = sum([len(values[parent]) for parent in parents[curr_var]])
+        q = q if q > 0 else 1
+        counts[index] = [None] * q
+        for i in range(len(counts[index])):
+            counts[index][i] = [0] * len(values[curr_var])
+
+    # * iterate through data to populate the counts
+    for row in data.iterrows():
+        # print("row " + str(row[0]) + "\n" + str(row[1]))
+        data_tuple = row[1]
+        for index in range(len(data_tuple)): # 0 -> n - 1
+            i, j, k = get_ijk_value(index, data_tuple, values, parents, nodes)
+            counts[i][j][k] += 1
+            # print("index", index, "i, j, k: ", i, j, k)
+
+    # print(counts)
+    
+    # * calculate bayesian score
+    p = 0
+    for i in range(len(nodes)):
+        for j in range(len(counts[i])):
+            row = counts[i][j]
+            p += loggamma(len(row))
+            p -= loggamma(len(row) + sum(row))
+            for k in range(len(row)):
+                p += loggamma(1 + row[k])
+                p -= loggamma(1)
+    return p
 
 def compute(infile, outfile):
-    # WRITE YOUR CODE HERE
-    # FEEL FREE TO CHANGE ANYTHING ANYWHERE IN THE CODE
-    # THIS INCLUDES CHANGING THE FUNCTION NAMES, MAKING THE CODE MODULAR, BASICALLY ANYTHING
+    # * process csv into graph
+    # data = pd.read_csv("data/large.csv", delimiter=",")
+    # var_names = data.columns
+    # graph = nx.Graph()
+    # graph.add_nodes_from(var_names)
+
+    # dummy test data A -> B <- C (example from video)
+    graph = nx.Graph()
+    graph.add_nodes_from(['A', 'B', 'C'])
+    graph.add_edges_from([('A', 'C'), ('B', 'C')])
+    data = pd.read_csv("data/test_data.csv", delimiter=",")
+    print(data)
+
+    for row in data.iterrows():
+        print(row)
+        print("...")
+
+    print("nodes: " + str(graph.nodes()))
+    print("edges: " + str(graph.edges()))
+
+    # * calculate bayesian score
+    score = bayesian_score(graph, data)
+    print("score: " + str(score))
+
+    # print("score: " + str(bayesian_score(test_graph, data)))
+
     pass
 
 
 def main():
-    if len(sys.argv) != 3:
-        raise Exception("usage: python project1.py <infile>.csv <outfile>.gph")
+    # if len(sys.argv) != 3:
+    #     raise Exception("usage: python project1.py <infile>.csv <outfile>.gph")
 
-    inputfilename = sys.argv[1]
-    outputfilename = sys.argv[2]
-    compute(inputfilename, outputfilename)
+    # inputfilename = sys.argv[1]
+    # outputfilename = sys.argv[2]
+    # compute(inputfilename, outputfilename)
+    compute(None, None)
 
 
 if __name__ == '__main__':
